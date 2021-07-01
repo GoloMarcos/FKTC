ebbc4caa959a17b3982fd110be8e8df3a301b4d5
Marcos Gôlo <68027659+GoloMarcos@users.noreply.github.com>
2021-07-01 14:33:51 -0300

    Update datasets.py

--- FakeNewsTextCollections/datasets.py
+++ FakeNewsTextCollections/datasets.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 from pathlib import Path
+from os.path import isfile
 
 def load(path_files=''):
 
@@ -8,26 +9,24 @@ def load(path_files=''):
   this function download and load all datasets and return a dictionary in which any key represent the dataset
   
   if the files already exist, to load them, pass the path where the files are through the path_files parameter
-  
-  this function changes the current directory, remember to go back to the directory you were in
   '''
 
-  if path_files != '':
-    os.chdir(path_files)
+  if not isfile(path_files + 'fakebr.plk'):
+    cmd = 'gdown --id 1TfBnP4-fUsIZtvDKtqXpUb4NvKLS5Kkz -O ' + path_files + 'fakebr.plk'
+    os.system(cmd) # FakeBr
 
-  if not Path('fakebr.plk').is_file():
-    os.system('gdown --id 1TfBnP4-fUsIZtvDKtqXpUb4NvKLS5Kkz') # FakeBr
-
-  if not Path('fcn.plk').is_file():
-    os.system('gdown --id 1-1GRgEOqAl46WX9yVLE6WEyDLtljVz7B') # FCN
+  if not isfile(path_files + 'fcn.plk'):
+    cmd = 'gdown --id 1-1GRgEOqAl46WX9yVLE6WEyDLtljVz7B -O ' + path_files + 'fcn.plk'
+    os.system(cmd) # FCN
   
-  if not Path('fnn.plk').is_file():
-    os.system('gdown --id 1--GbGtCP3OCExNmdlgU-M7BHa9FMIHaA')  # FNN
+  if not isfile(path_files + 'fnn.plk'):
+    cmd = 'gdown --id 1--GbGtCP3OCExNmdlgU-M7BHa9FMIHaA -O ' + path_files + 'fnn.plk'
+    os.system(cmd)  # FNN
 
   bases = {
-    'fcn' : pd.read_pickle('fcn.plk'),
-    'fakebr' : pd.read_pickle('fakebr.plk'),
-    'fnn' : pd.read_pickle('fnn.plk')
+    'fcn' : pd.read_pickle(path_files + 'fcn.plk'),
+    'fakebr' : pd.read_pickle(path_files + 'fakebr.plk'),
+    'fnn' : pd.read_pickle(path_files + 'fnn.plk')
   }
 
   return bases
